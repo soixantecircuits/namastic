60d8e476ceefded2ea0f08ba9c200d320938ef93
gabrielstuff <gabriel@soixantecircuits.fr>
2016-10-13 20:00:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// https://en.wikipedia.org/wiki/Category:Arabic_feminine_given_names
+// https://en.wikipedia.org/wiki/List_of_most_common_surnames_in_North_America
+
+var fnameFr = require('./i18n/fname/fr.json')
+var fnameAr = require('./i18n/fname/ar.json')
+var nameFr = require('./i18n/name/fr.json')
+
+var fname = [...fnameFr, ...fnameAr]
+
+module.exports = {
+  fname: fname,
+  name: nameFr
+}
---
+++ test/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var names = require('../')
+
+console.log(names)
