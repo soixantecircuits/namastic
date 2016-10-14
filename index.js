131cd8c7b28be75f1bf62d31c322ffa6ec8eb5c0
gabrielstuff <gabriel@soixantecircuits.fr>
2016-10-14 21:43:06 +0200

    add first european name

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // https://en.wikipedia.org/wiki/Category:Arabic_feminine_given_names
 // https://en.wikipedia.org/wiki/List_of_most_common_surnames_in_North_America
+// https://en.wikipedia.org/wiki/List_of_most_popular_given_names#Europe
 
 var fnameFr = require('./i18n/fname/fr.json')
 var fnameAr = require('./i18n/fname/ar.json')
