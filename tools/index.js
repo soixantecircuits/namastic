08727786715acb32d69fadc73a7094329df34ace
gabrielstuff <gabriel@soixantecircuits.fr>
2016-10-14 22:48:25 +0200

    ✍️ adding male 🔝top 10 common first names

--- tools/index.js
+++ tools/index.js
@@ -1,3 +1,19 @@
+'use strict'
+
 const names = require('../i18n/fname/europe.json')
 const fs = require('fs')
+const countries = require('i18n-iso-countries')
 const _ = require('lodash')
+
+
+for (let countryCode in names){
+  let countryNames = names[countryCode]
+  var country = countries.getName(countryCode, 'en')
+  _.each(countryNames, function (el, index) {
+    countryNames[index] = {name: el, gender: 'male', nationality: country}
+  })
+  fs.writeFile(`./i18n/fname/exports/${countryCode}.json`, JSON.stringify(countryNames, null, 2), (err) => {
+    if (err) throw err
+    console.log(`${countryCode} It\'s saved!`)
+  })
+}
